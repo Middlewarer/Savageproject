08c7106051f1aee56adb57e5de601c1384a984f8
Middlewarer <mranarchy@mail.ru>
2024-05-05 11:06:46 +0300

    true another commit

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
+    path('nh/', views.NotHomeView.as_view(), name='nh'),
 ]
 
 
--- main/views.py
+++ main/views.py
@@ -5,3 +5,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'main/index.html'
+
+class NotHomeView(TemplateView):
+    template_name = 'main/not_home.html'
